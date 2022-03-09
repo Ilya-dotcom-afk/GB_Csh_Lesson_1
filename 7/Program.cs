be8124026f4059407c4e9416e2590126f292a29d
Ilya <ilya_rogachev_89@mail.ru>
2022-03-09 18:44:30 +0300

    7

---
+++ 7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("введите трёхзначное число");
+double a = Convert.ToDouble(Console.ReadLine());
+double b, d;
+if (a>99 && a<1000)
+{
+b = a%10;
+d = (a - a%100)/100;
+Console.WriteLine(d*10+b);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
