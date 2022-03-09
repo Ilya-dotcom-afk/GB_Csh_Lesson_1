61b44aacba187239d63e0304165d6f9422f08f23
Ilya <ilya_rogachev_89@mail.ru>
2022-03-09 17:56:18 +0300

    5

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("введите трёхзначное число");
+double a = Convert.ToDouble(Console.ReadLine());
+double b;
+if (a>99 && a<1000)
+{
+b = (a%100 - a%10)/10;
+Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
