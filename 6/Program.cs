f98aa88a3be6258897cc1e66de779b0332bc8a0d
Ilya <ilya_rogachev_89@mail.ru>
2022-03-09 18:07:45 +0300

    6

---
+++ 6/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.WriteLine("введите число от 10 до 99 включительно");
+double a = Convert.ToDouble(Console.ReadLine());
+double b, c;
+if (a <= 99 && a >= 10)
+{
+b = a%10;
+c = (a - b)/10;
+if (c>b){
+Console.WriteLine(c);
+}
+if (c<b){
+    Console.WriteLine(b);
+}
+if (c==b){
+    Console.WriteLine("цифры равны");
+}
+}
+else
+{
+    Console.WriteLine("Число не входит в заданный интервал");
+}
