c64a5aee25c31f689d81c94b810b0a8f46c1b529
Ilya <ilya_rogachev_89@mail.ru>
2022-03-09 18:55:09 +0300

    8

---
+++ 8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число, проеряемое на кратность");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите кратность");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a%b != 0){
+Console.WriteLine("число " + a + " не кратно числу " + b + " остаток от деления равен " + a%b);
+}
+else{
+    Console.WriteLine("число " + a + " нацело делится на " + b);
+}
